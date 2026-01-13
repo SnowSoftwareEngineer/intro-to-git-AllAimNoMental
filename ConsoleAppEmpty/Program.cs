12ccfb553ceae6c2dc1be750a55cdbe1e04cef90
AllAimNoMental <luis.colmenares@ENGR5F8P5S3>
2026-01-13 14:31:27 -0700

    Almost final code

--- ConsoleAppEmpty/Program.cs
+++ ConsoleAppEmpty/Program.cs
@@ -14,6 +14,13 @@
 
 
         Console.WriteLine("Changed it , hurray!!");
+        int a = 5;
+        int b = 10;
+        int result = a + b;
+
+        Console.WriteLine("Five plus 10 is " + result);
+
+
 
 
     }
